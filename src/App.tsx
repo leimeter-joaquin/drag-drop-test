add82355f88b62f203f3f51be972915e0b5b7f26
leimeter-joaquin <leimeter.joaquin@gmail.com>
2025-03-28 14:54:32 -0300

    add drag drop example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
+import { DndContext, DragOverEvent, UniqueIdentifier } from "@dnd-kit/core";
+
+import { Droppable } from "./Droppable";
+import { Draggable } from "./Draggable";
+
 function App() {
+  const containers = ["A", "B", "C"];
+  const [parent, setParent] = useState<UniqueIdentifier | null>(null);
+  const draggableMarkup = <Draggable id="draggable">Drag me</Draggable>;
+
   return (
-    <>
-      <div>
-        <h1 className="bg-red-500">Drag and Drop Test</h1>
-      </div>
-    </>
+    <DndContext onDragEnd={handleDragEnd}>
+      {parent === null ? draggableMarkup : null}
+
+      {containers.map((id) => (
+        // We updated the Dropable component so it would accept an `id`
+        // prop and pass it to `useDropable`
+        <Droppable key={id} id={id}>
+          {parent === id ? draggableMarkup : "Drop here"}
+        </Droppable>
+      ))}
+    </DndContext>
   );
+
+  function handleDragEnd(event: DragOverEvent) {
+    const { over } = event;
+
+    // If the item is dropped over a container, set it as the parent
+    // otherwise reset the parent to `null`
+    setParent(over ? over.id : null);
+  }
 }
 
 export default App;
